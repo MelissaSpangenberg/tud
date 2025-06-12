9a776abdd49260a0816c34de3785ffa9fcbf535f
MelissaSpangenberg <141252662+MelissaSpangenberg@users.noreply.github.com>
2025-06-12 13:00:51 +0000

    added show back

--- email-concierge-frontend/src/emailList.jsx
+++ email-concierge-frontend/src/emailList.jsx
@@ -1,5 +1,5 @@
 import { List, Datagrid, TextField } from "react-admin";
-// import { Show, SimpleShowLayout } from 'react-admin';
+import { Show, SimpleShowLayout } from 'react-admin';
 
 export const emailList = () => (
     <List>
@@ -14,15 +14,15 @@ export const emailList = () => (
     </List>
 );
 
-// export const emailShow = () => (
-//     <Show>
-//         <SimpleShowLayout>
-//             <TextField source="id" label="ID" />
-//             <TextField source="emailClass" label="Email Class" />
-//             <TextField source="sentiment" label="Sentiment" />
-//             <TextField source="urgency" label="Urgency" />
-//             <TextField source="draft" label="Draft Reponse" />
-//             <TextField source="sanitized" label="Sanitized" />
-//         </SimpleShowLayout>
-//     </Show>
-// );
+export const emailShow = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="id" label="ID" />
+            <TextField source="emailClass" label="Email Class" />
+            <TextField source="sentiment" label="Sentiment" />
+            <TextField source="urgency" label="Urgency" />
+            <TextField source="draft" label="Draft Reponse" />
+            <TextField source="sanitized" label="Sanitized" />
+        </SimpleShowLayout>
+    </Show>
+);
